eb029e0ffac474265ee691aa504fa2fabb124bcf
jluna <jluna@JAMES-LAPTOP>
2024-01-01 12:33:00 -0700

    Write CompressStrategy trait for handling compressions.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate uuid;
 use std::process::id;
 use uuid::Uuid;
+use std::io::prelude::*;
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
 
 struct File {
     file_id: Uuid,
@@ -10,6 +13,19 @@ impl File {
     fn new(file_data: String) -> File {
         let file_id: Uuid = Uuid::new_v4();
         File { file_id, file_data }
+    }
+}
+trait CompressStrategy {
+    fn compress(&self, input: &str) -> &str;
+}
+struct JsonCompressor;
+impl CompressStrategy for JsonCompressor {
+    fn compress(&self, input: &str) -> &str {
+        let mut e = zlibEncoder::new(Vec::new(), Compression::default());
+        e.write_all(input);
+        let compressed_bytes = e.finish();
+        let str = "hello";
+        return input;
     }
 }
 fn main() {

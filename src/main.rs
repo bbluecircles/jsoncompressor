08fc59e92a18c2b1bd6c78bcdebf0e6c7e3be170
jluna <jluna@JAMES-LAPTOP>
2024-01-01 10:47:11 -0700

    Include UUID package. Create struct and impl for Files.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate uuid;
+use std::process::id;
+use uuid::Uuid;
+
+struct File {
+    file_id: Uuid,
+    file_data: String
+}
+impl File {
+    fn new(file_data: String) -> File {
+        let file_id: Uuid = Uuid::new_v4();
+        File { file_id, file_data }
+    }
+}
 fn main() {
     println!("Hello, world!");
 }

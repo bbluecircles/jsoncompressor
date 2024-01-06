37e6faee664a33174139ed13471fc344e2538289
bbluecircles <dev.jamesluna@gmail.com>
2024-01-06 14:29:01 -0700

    For further development, test by passing files into terminal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate uuid;
 use std::fmt::Error;
+use std::io;
+use std::io::prelude::*;
+use std::fs;
 use uuid::Uuid;
 use std::io::prelude::*;
 use flate2::Compression;
@@ -32,5 +35,21 @@ pub fn compress_json(file_content: &[u8]) -> Result<Vec<u8>, std::io::Error> {
     return compress_strategy.compress(file_content);
 }
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter a JSON file you'd like to compress.");
+    let mut file_path = String::new();
+    // Read input from terminal.
+    io::stdin().read_line(&mut file_path)
+    .expect("Failed to read the file.");
+    // Trim the newline character at the end.
+    file_path = file_path.trim_end().to_string();
+    // Ensure content has been read.
+    let content: Vec<u8> = fs::read(&file_path)
+        .expect("Filed to read the file");
+    // Print the number of bytes read before the compression.
+    println!("Number of bytes read before parse: {}", content.len());
+    // Try to compress file.
+    let result: Result<Vec<u8>, io::Error> = compress_json(&content);
+    let vec = result.expect("Failed to compress JSON file.");
+    // Print the number of bytes after compressing.
+    println!("Number of bytes read after parse: {}", vec.len());
 }
